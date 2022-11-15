ba39560000e787b7b86340f327927b9b6c27f473
Dinindu Seneviratne <dininduviduneth@gmail.com>
2022-11-15 23:49:29 +0100

    Add support for histogram generation :white_check_mark:

--- data_analysis/da_dinindu_v1.py
+++ data_analysis/da_dinindu_v1.py
@@ -1,5 +1,12 @@
-from utils import loading_data
+import sys
+sys.path.append("..")
+
+from utils.loading_data  import load_to_df_from_csv
+from utils.plotting import draw_histogram
 
 # Loading datasets
-train = loading_data.load_to_df_from_csv("../data/train.csv")
-test = loading_data.load_to_df_from_csv("../data/test.csv")
+test = load_to_df_from_csv("../data/test.csv")
+train = load_to_df_from_csv("../data/train.csv")
+
+# Histogram of movies per year
+draw_histogram(train.Year, "Year", "Movies", "Movies Across Years")
---
+++ utils/plotting.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+def draw_histogram(data, xlabel, ylabel, title):
+    """
+    Draws a basic histogram
+    Arguments:
+        data: an array of datapoints
+        xlabel: label for X-axis
+        ylabel: label for Y-axis
+        title: title of the histogram
+    Returns:
+        No return - only plots the histogram
+    """
+
+    plt.hist(data)
+
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+
+    plt.show()
